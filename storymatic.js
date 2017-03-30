47cde0209f25fca3f72db7f2e4b87c18de9e49b0
joshua levin <joshua.c.levin@gmail.com>
2017-03-30 23:11:08 +0100

    questions working

--- storymatic.js
+++ storymatic.js
@@ -1,3 +1,4 @@
+'use strict';
 // Filesystem
 let fs = require('fs');
 // Readline
@@ -5,14 +6,18 @@ let readline = require('readline');
 // Arrays
 let peopleCards = [];
 let thingCards = [];
-// Setup readline
-let readPeople = readline.createInterface({
-    input: fs.createReadStream('./peopleCards.txt')
-});
-let readThings = readline.createInterface({
-    input: fs.createReadStream('./thingCards.txt')
-});
-// Taken from http://stackoverflow.com/a/1527820
+// Inquirer for prompts
+let inquirer = require('inquirer');
+// Inquirer prompts
+let questions = [
+    {
+        type: 'list',
+        name: 'card',
+        message: 'pick a card',
+        choices: ['Person', 'Thing']
+    }
+];
+// Gets a random integer within a range, taken from http://stackoverflow.com/a/1527820
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + 1) + min;
 }
@@ -23,19 +28,40 @@ module.exports = {
     },
     getThing: () => {
         return thingCards[getRandomInt(0, thingCards.length - 1)];
+    },
+    askQuestion: () => {
+        inquirer.prompt(questions).then((answers) => {
+            let response = (answers.card === 'Person') ? module.exports.getPerson() : module.exports.getThing();
+            console.log(`${answers.card} card: ${response}`);
+            module.exports.askQuestion();
+        });
+    },
+    // Setup readline
+    readInPeople: () => {
+        let readPeople = readline.createInterface({
+            input: fs.createReadStream('./peopleCards.txt')
+        });
+        // Read in peopleCards;
+        readPeople.on('line', (line) => {
+            peopleCards.push(line);
+        });
+        readPeople.on('close', () => {
+            console.log('random card from list of', peopleCards.length);
+            module.exports.readInThings();
+        });
+    },
+    readInThings: () => {
+        let readThings = readline.createInterface({
+            input: fs.createReadStream('./thingCards.txt')
+        });
+        // Read in thingCards;
+        readThings.on('line', (line) => {
+            thingCards.push(line);
+        });
+        readThings.on('close', () => {
+            console.log('random card from list of', peopleCards.length);
+            module.exports.askQuestion();
+        });
     }
-}
-// Read in peopleCards;
-readPeople.on('line', (line) => {
-    peopleCards.push(line);
-});
-readPeople.on('close', () => {
-    console.log('random card from list of',peopleCards.length,'is:', module.exports.getPerson());
-});
-// Read in thingCards;
-readThings.on('line', (line) => {
-    thingCards.push(line);
-});
-readThings.on('close', () => {
-    console.log('random card from list of',peopleCards.length,'is:', module.exports.getThing());
-});
+};
+module.exports.readInPeople();
